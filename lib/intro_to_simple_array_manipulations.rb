2e95571d0990674f99f9d000f3ed23342b63a2fa
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-24 15:30:22 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,11 +1,9 @@
 def using_push(array, element)
-   colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-   colors_in_the_rainbow.push("violet")
+  array.push(element)
   end
 
 def using_unshift(array, element)
-  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-  bouroughs_in_nyc.unshift("Staten Island")
+  array.unshift(element)
 end
 
 def using_pop(array)
